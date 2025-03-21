bc3eda1ff09a4f887e5a616a72f5bdb8be94ff68
Reymí López Mena <123846607+reymi93@users.noreply.github.com>
2025-03-21 16:21:42 -0400

    push

--- lib/constansts/index.ts
+++ lib/constansts/index.ts
@@ -1,7 +1,7 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "JoinMarket";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  "Una moderna tienda con precios competivivos para Cuba";
+  "Una moderna tienda con precios competitivos para Cuba";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT = Number(
